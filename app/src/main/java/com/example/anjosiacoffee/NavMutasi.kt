904a8d0554915adb628af5f3c3d22e02858fd63a
AyuLintang03 <ayulintang95@gmail.com>
2022-11-29 11:19:49 +0700

    Aplikasi_Anjosia_Coffee_ke_2

--- app/src/main/java/com/example/anjosiacoffee/NavMutasi.kt
+++ app/src/main/java/com/example/anjosiacoffee/NavMutasi.kt
@@ -1,17 +1,25 @@
 package com.example.anjosiacoffee
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 
 @Composable
 fun NavMutasi(){
@@ -21,7 +29,6 @@ fun NavMutasi(){
             .verticalScroll(rememberScrollState())
     ) {
 
-
         Card(
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier
@@ -30,14 +37,61 @@ fun NavMutasi(){
         ) {
             Box(
                 modifier = Modifier
-                    .height(200.dp)
+                    .height(250.dp)
                     .padding(16.dp)
             ) {
                 Text("Cari Mutasi")
                 var title3 by remember { mutableStateOf(TextFieldValue("")) }
+                var mExpanded by remember { mutableStateOf(false) }
+
+                val stransaksi = listOf("Pengeluaran", "Penjualan")
+
+                var mSelectedText by remember { mutableStateOf("") }
+
+                var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
                 Column(
-                    Modifier.padding(top = 40.dp)
+                    Modifier.padding(top = 20.dp)
                 ) {
+                    val icon = if (mExpanded)
+                        Icons.Filled.KeyboardArrowUp
+                    else
+                        Icons.Filled.KeyboardArrowDown
+
+
+                        OutlinedTextField(
+                            value = mSelectedText,
+                            onValueChange = { mSelectedText = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .onGloballyPositioned { coordinates ->
+                                    // This value is used to assign to
+                                    // the DropDown the same width
+                                    mTextFieldSize = coordinates.size.toSize()
+                                },
+                            label = {Text("Pilih Transaksi")},
+                            trailingIcon = {
+                                Icon(icon,"contentDescription",
+                                    Modifier.clickable { mExpanded = !mExpanded })
+                            }
+                        )
+                        DropdownMenu(
+                            expanded = mExpanded,
+                            onDismissRequest = { mExpanded = false },
+                            modifier = Modifier
+                                .width(with(LocalDensity.current){mTextFieldSize.width.toDp()})
+                        ) {
+                            stransaksi.forEach { label ->
+                                DropdownMenuItem(onClick = {
+                                    mSelectedText = label
+                                    mExpanded = false
+                                }) {
+                                    Text(text = label)
+                                }
+                            }
+                        }
+
+
+
                     OutlinedTextField(
                         value = title3,
                         label = { Text(text = "Tanggal") },
@@ -70,7 +124,7 @@ fun NavMutasi(){
         }
     }
 
-    Box(modifier = Modifier.padding(top = 270.dp) ) {
+    Box(modifier = Modifier.padding(top = 350.dp) ) {
         Card(
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier
